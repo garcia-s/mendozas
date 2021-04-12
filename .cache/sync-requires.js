b2f17cbd37ae5fca538f16c64b3104c65463740a
Juan García <43949658+garcia-s@users.noreply.github.com>
2021-04-11 19:11:41 -0500

    changed to gatsby

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/symmetrytech/Documents/GitHub/mendozas/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/symmetrytech/Documents/GitHub/mendozas/src/pages/index.js"))
+}
+
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Contact from '../Contact';
+import Footer from '../Footer';
+import Main from '../Main';
+import Team from '../Team';
+
+function App() {
+  return (
+    <>
+      <Main />
+      <Team />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+
+// AIzaSyDtHk7SQZFR9oeYyZvWgnUFwr17KV5_5gM
